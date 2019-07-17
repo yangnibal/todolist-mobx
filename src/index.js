d0d07f7275fad6a862726baa68313cc5dc3f29cb
yangnibal <wonjun9670@naver.com>
2019-07-17 09:00:36 +0900

    error

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'mobx-react';
+import MarketStore from './store/market';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const market = new MarketStore();
+
+ReactDOM.render(
+    <Provider market={market}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
